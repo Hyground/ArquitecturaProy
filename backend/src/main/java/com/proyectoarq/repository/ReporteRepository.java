e2a707734a0933fd45c5dbb31f8892654e1570ce
Hyground <hygroundd@gmail.com>
2026-05-23 17:54:21 -0600

    reinicio

--- backend/src/main/java/com/proyectoarq/repository/ReporteRepository.java
+++
@@ -1,7 +0,0 @@
-package com.proyectoarq.repository;
-
-import com.proyectoarq.model.Reporte;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReporteRepository extends JpaRepository<Reporte, Long> {
-}
